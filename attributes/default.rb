61b30e70520a2908631eecd456481e41f19975cf
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2017-02-02 13:21:56 -0800

    update plexapp version to 1.3.4.3285-b46e0ea

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,4 @@
-# default['plexapp']['version'] = '0.9.12.3.1173-937aac3'
-#default['plexapp']['version'] = '0.9.14.6.1620-e0b7243'
-default['plexapp']['version'] = '1.3.2.3112-1751929'
+default['plexapp']['version'] = '1.3.4.3285-b46e0ea'
 
 # default['media']['aws_secret_access_key']
 # default['media']['aws_access_key_id']
--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name 'mediasrv'
-version '0.1.12'
+version '0.1.13'
 
 depends 'plexapp'
 depends 'firewall'
